eceeaf92e4678ff6f9ed101e66afe57f19374882
Edu Caro <eduardo.caro.lorente@gmail.com>
2024-02-05 08:40:56 +0100

    Ejercicio 4

--- Funciones.java
+++ Funciones.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Funciones {
 
     public static void mostrarVectorInt(int []vector){
@@ -12,7 +14,27 @@ public class Funciones {
         System.out.println("]");
     }
     public static void mostrarMatrizInt(int [][]matriz){
-        
+        for (int i=0;i<matriz.length; i++){
+            Funciones.mostrarVectorInt(matriz[i]);
+        }
+    }
+    public static void copiar2vectores(int []vector1,int []vector2){
+        System.out.println("Actualmente el vector 2 es "+Arrays.toString(vector2));
+        if(vector1.length == vector2.length){
+            for(int i =0;i<vector1.length;i++){
+                vector2[i] = vector1[i];
+            }
+            System.out.println("Ahora el vector 2 es "+Arrays.toString(vector2));
+            System.out.println("----------");
+        }else {
+            System.out.println("Los tamaños de los vectores son diferentes y no se puede llevar a cabo");
+            System.out.println("----------");
+        }
+    }
+    public static int[] copiarVector(int []vector1){
+        int []vector2 = new int [vector1.length];
+        Funciones.copiar2vectores(vector1,vector2);
+        return vector2;
     }
 
     }
--- Main.java
+++ Main.java
@@ -1,18 +1,33 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         int opcion;
+        int [] vectorCopiado;
         int [] array1 = new int []{1,2,3,4,5};
+        int [] array2 = new int []{2,3,4};
+        int [] array3 = new int []{3,4,5,6,7};
         int [][] matriz1 = new int [][]{{1,2,3},{4,5,6},{7,8,9}};
         do{
             System.out.println("1. Ejercicio 1 - Mostrar Vector");
+            System.out.println("2. Ejercicio 2 - Mostrar Matriz");
+            System.out.println("3. Ejercicio 3 - Copiar dos vectores");
+            System.out.println("4. Ejercicio 4 - Copiar un único vector");
             opcion = Leer.leerEntero("Introduce una opción: ");
             switch(opcion){
                 case 1:
                     Funciones.mostrarVectorInt(array1);
                     break;
                 case 2:
-
+                    Funciones.mostrarMatrizInt(matriz1);
+                    break;
+                case 3:
+                    Funciones.copiar2vectores(array1,array2);
+                    Funciones.copiar2vectores(array1,array3);
+                    break;
+                case 4:
+                    vectorCopiado = Funciones.copiarVector(array3);
+                    
             }
         }while(opcion != 0);
 
